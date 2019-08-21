cebfa9d5ec72ca9f1be0a19b004717e2c3783ebd
Matthew Pendleton <mapendleton37@gmail.com>
2019-08-20 20:27:21 -0500

    added IPlayer, Deck, Card classes

---
+++ CardGames/CardGames/Deck.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardGames
+{
+
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
+                    Cards.Add(card);
+                }
+            }
+        }
+
+        public List<Card> Cards { get; set; }
+
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> tempList = new List<Card>();
+                Random rand = new Random();
+
+                while(this.Cards.Count > 0)
+                {
+                    int randomIndex = rand.Next(0, this.Cards.Count);
+                    tempList.Add(this.Cards[randomIndex]);
+                    this.Cards.RemoveAt(randomIndex);
+                }
+                this.Cards = tempList;
+            }
+        }
+    }
+}
---
+++ CardGames/CardGames/IPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardGames
+{
+
+    interface IPlayer
+    {
+        int Name { get; set; }
+        List<Card> Hand { get; set; }
+        bool IsActivelyPlaying { get; set; }
+        bool IsTurn { get; set; }
+        Guid PlayerId { get; set; }
+
+        void Draw();
+        
+    }
+}
